f4ba700a928daad36d6e7be1e48e0fce495a8aac
Bartek Filipiuk <bartekdevins@gmail.com>
2025-12-16 21:14:23 +0100

    fix: add migration for footer display options columns

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251207_174832 from './20251207_174832';
+import * as migration_20251216_201335 from './20251216_201335';
 
 export const migrations = [
   {
     up: migration_20251207_174832.up,
     down: migration_20251207_174832.down,
-    name: '20251207_174832'
+    name: '20251207_174832',
+  },
+  {
+    up: migration_20251216_201335.up,
+    down: migration_20251216_201335.down,
+    name: '20251216_201335'
   },
 ];
